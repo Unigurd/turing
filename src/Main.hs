8e5eb0abe32339fac055c7be5efe9adb5051a187
Sigurd Dam Sonniks <lsn777@alumni.ku.dk>
2020-01-08 22:05:08 +0100

    Added comments

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,59 @@
 import Prelude hiding (Left, Right, pred)
 import Turing as T
 
-data Symbol = A Integer | Heart | V | H | B deriving (Show, Eq)
-data State  = Start | Q1 Integer | Q2 Integer | Q3 | Q4 | Stop deriving Eq
+-- This is a machine from some past assignment. Just to check my implementation
+-- It verifies words of the form 
+-- A1 A2 A3 ... An Heart A1 A2 A3 ... An
+-- The numbers do not neccessarily have to come in numerical order
+-- if the word fits, the tape will look like 
+-- V  V  V  ... V  Heart H  H  H  ... H
+-- after the computation has finished
 
-myPred = (/=Stop)
+-- Read the explanation for delta
+data Symbol = A Integer | Heart | V | H | B deriving (Show, Eq)
+data State  = Start | Q1 Integer | Q2 Integer | Q3 | Q4 | Failure | Finish deriving (Show, Eq)
+
+-- The state Stop is the only stopping state
+myPred state = foldl (\acc elm -> (elm /= state) && acc) True [Failure, Finish]
+
+-- This function and explanation is organized according to the happy path in the computation
+-- First we read some symbol An and write V to indicate we have been here
 delta Start (A n) = ((Q1 n), V, Right)
-delta Start Heart = (Q4, Heart, Right)
+-- Then we head left over all the symbols on the left side of the heart
 delta q@(Q1 _) a@(A _) = (q, a, Right)
+-- When reaching the heart we switch state
 delta (Q1 n) Heart = ((Q2 n), Heart, Right)
-delta (Q2 n) (A m) = if n == m then (Q3, H, Left) else (Stop, (A m), Stay)
+-- and keeping heading right over all the H's,
+-- which indicate (A n)'s we've already done.
+-- There are no H's in the beginning
 delta q@(Q2 _) H = (q, H, Right)
-delta Q3 a@(A _) = (Q3, a, Left)
-delta Q3 Heart   = (Q3, Heart, Left)
-delta Q3 V       = (Start, V, Right)
-delta Q3 H       = (Q3, H, Left)
-delta Q4 H       = (Q4, H, Right)
-delta _  symbol  = (Stop, symbol, Stay)
+-- When we find the first A m on the right side of the heart,
+-- we write a H if m is the same as the n we found in the beginning
+-- (which we carried in our state).
+-- If it isn't, we stop prematurely
+delta (Q2 n) (A m) = if n == m then (Q3, H, Left) else (Failure, (A m), Stay)
+-- We then start heading left over the H's
+delta Q3 H         = (Q3, H, Left)
+-- and over the heart
+delta Q3 Heart    = (Q3, Heart, Left)
+-- and over the untreated A's to the left of the heart.
+delta Q3 a@(A _)   = (Q3, a, Left)
+-- When we find the first V, we know everything to the left is done,
+-- and so we prepare to repeat everything on the symbol to the right.
+delta Q3 V         = (Start, V, Right)
+-- If the first symbol we see when we repeat is the heart,
+-- we only have left to check that there aren't any extra A's
+-- on the right side
+delta Start Heart  = (Q4, Heart, Right)
+-- so we walk past all the H's
+delta Q4 H         = (Q4, H, Right)
+-- If the first non-H symbol we find is the blank symbol,
+-- the given word has the right form.
+delta Q4 B         = (Finish, B, Stay)
+-- If anything else happens, the word is not of the correct form,
+-- and we exit with failure.
+delta _  symbol    = (Failure, symbol, Stay)
+
 
                                                     -- Difference is here        
                                                     --       |  
--- src/Turing.hs
+++ src/Turing.hs
@@ -9,39 +9,52 @@ data Machine state symbol =
           , pred   :: (state -> Bool)
           , fun    :: (state -> symbol -> (state, symbol, Step)) 
           }
+
+-- The tape, with the current symbol between the left and right symbols
 data Tape a = Tape [a] a [a] deriving Eq
 instance Foldable Tape where
   foldMap f (Tape ls elm rs) = foldMap f (reverse ls) `mappend` f elm `mappend` foldMap f rs
+
 
 removeEmptySymbol emptySymbol [elm] = if emptySymbol == elm then [] else [elm]
 removeEmptySymbol _           list  = list
 remEmptSym :: Eq a => a -> [a] -> [a]
 remEmptSym = removeEmptySymbol
 
+-- Move the position on the tape, cleaning up trailing/leading empty symbols
 move es Left  (Tape [] elm rs)       = Tape [] es (elm:(remEmptSym es rs))
 move es Left  (Tape (l:ls) elm rs)   = Tape ls l (elm:(remEmptSym es rs))
 move _  Stay  tape                   = tape
 move es Right (Tape ls elm [])       = Tape (elm:(remEmptSym es ls)) es []
 move es Right (Tape (ls) elm (r:rs)) = Tape (elm:(remEmptSym es ls)) r rs
 
+-- Change the symbol under the head
 change (Tape ls _ rs) newSymbol = (Tape ls newSymbol rs)
 
+-- Get current symbol
 currentSymbol (Tape _ elm _) = elm
 
+-- Convert list to tape
 list2Tape (elm:rs) = Tape [] elm rs
 
-turing :: Eq symbol => Machine state symbol -> [symbol] -> [symbol]
-turing machine rawTape = toList $ result
+-- Takes a turing machine and a tape, and returns the resulting tape and state of machine
+turing :: Eq symbol => Machine state symbol -> [symbol] -> (state,[symbol])
+turing machine rawTape = (endState, toList $ endTape) -- Convert the tape to [Symbol] before returning
   where
-    result = runTuring state tape
-    tape = list2Tape rawTape
-    state = startState machine
-    eSym = emptySymbol machine
-    p = pred machine
-    f = fun machine
+    -- The results
+    (endState, endTape) = runTuring state tape
+    -- converts the tape from [symbol] to Tape symbol
+    tape   = list2Tape rawTape
+    -- Gets the different things out of the machine
+    state  = startState machine
+    eSym   = emptySymbol machine
+    p      = pred machine
+    f      = fun machine
+    -- If the predicate is false, the machine is in an end state and we thus return.
+    -- Otherwise we continue
     runTuring state tape = if p newState 
                            then runTuring newState newTape
-                           else newTape
+                           else (newState,newTape)
       where
         (newState, newSymbol, step) = f state $ currentSymbol tape
         newTape = move eSym step $ change tape newSymbol
